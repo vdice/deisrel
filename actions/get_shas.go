6af8562c5dcc8de8a9f551b9d8e71352d2c33ccb
Aaron Schlesinger <aschlesinger@deis.com>
2016-04-14 15:46:42 -0700

    authenticating GH requests

--- actions/get_shas.go
+++ actions/get_shas.go
@@ -15,44 +15,45 @@ const (
 )
 
 // GetShas is the CLI action for getting github shas of all of the Deis Workflow repos
-func GetShas(c *cli.Context) {
-	ghClient := github.NewClient(nil)
-	outCh := make(chan string)
-	var wg sync.WaitGroup
-	for _, repo := range repoNames {
-		wg.Add(1)
-		ch := make(chan string)
-		go func(repo string) {
-			defer wg.Done()
-			repoCommits, _, err := ghClient.Repositories.ListCommits("deis", repo, &github.CommitsListOptions{
-				ListOptions: github.ListOptions{
-					Page:    1,
-					PerPage: 1,
-				},
-			})
-			if err != nil {
-				log.Fatalf("Error listing commits for repo %s (%s)", repo, err)
-			}
-			if len(repoCommits) < 1 {
-				log.Fatalf("No commits found for repo %s", repo)
-			}
-			repoCommit := repoCommits[0]
-			sha := *repoCommit.SHA
-			if c.Bool(ShortFlag) {
-				sha = sha[:7]
-			}
-			ch <- fmt.Sprintf("%s: %s\n", repo, sha)
-		}(repo)
+func GetShas(ghClient *github.Client) func(c *cli.Context) {
+	return func(c *cli.Context) {
+		outCh := make(chan string)
+		var wg sync.WaitGroup
+		for _, repo := range repoNames {
+			wg.Add(1)
+			ch := make(chan string)
+			go func(repo string) {
+				defer wg.Done()
+				repoCommits, _, err := ghClient.Repositories.ListCommits("deis", repo, &github.CommitsListOptions{
+					ListOptions: github.ListOptions{
+						Page:    1,
+						PerPage: 1,
+					},
+				})
+				if err != nil {
+					log.Fatalf("Error listing commits for repo %s (%s)", repo, err)
+				}
+				if len(repoCommits) < 1 {
+					log.Fatalf("No commits found for repo %s", repo)
+				}
+				repoCommit := repoCommits[0]
+				sha := *repoCommit.SHA
+				if c.Bool(ShortFlag) {
+					sha = sha[:7]
+				}
+				ch <- fmt.Sprintf("%s: %s\n", repo, sha)
+			}(repo)
+			go func() {
+				outCh <- <-ch
+			}()
+		}
 		go func() {
-			outCh <- <-ch
+			wg.Wait()
+			close(outCh)
 		}()
-	}
-	go func() {
-		wg.Wait()
-		close(outCh)
-	}()
 
-	for str := range outCh {
-		fmt.Print(str)
+		for str := range outCh {
+			fmt.Print(str)
+		}
 	}
 }
--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/arschles/deisrel/actions"
 	"github.com/codegangsta/cli"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 func main() {
+	ghTkn := os.Getenv("GITHUB_TOKEN")
+	if ghTkn == "" {
+		log.Fatalf("'GITHUB_TOKEN' env var required")
+	}
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: ghTkn})
+	cl := oauth2.NewClient(oauth2.NoContext, ts)
+	ghClient := github.NewClient(cl)
+
 	app := cli.NewApp()
 	app.Name = "deisrel"
 	app.Usage = "Utilities for releasing a new Deis version"
 	app.Commands = []cli.Command{
 		cli.Command{
 			Name:   "shas",
-			Action: actions.GetShas,
+			Action: actions.GetShas(ghClient),
 			Flags: []cli.Flag{
 				cli.BoolFlag{
 					Name:  actions.ShortFlag,
